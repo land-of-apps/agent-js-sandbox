f2616e025006792f0756252cd313fcf5aababc77
Laurent Christophe <lachrist@vub.ac.be>
2021-08-12 15:52:45 +0200

    tweak of the configuration file

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ git clone https://github.com/bkimminich/juice-shop.git
 cd juice-shop
 npm install
 npm install @appland/appmap-agent-js
+ln -s ../appmap.yml appmap.yml
 mkdir tmp
 mkdir tmp/appmap
-cp ../appmap.yml appmap.yml
 npx @appland/appmap-agent-js --scenario juice
